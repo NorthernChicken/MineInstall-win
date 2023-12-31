245ec1b4836826355b3d7f45e3ec4c4af1ffa8fa
NorthernChicken <144752748+NorthernChicken@users.noreply.github.com>
2023-12-31 14:22:03 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -9,61 +9,72 @@
 $server_version = "1.20.4"
 $server_link = "https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar"
 
+$java_installed = $false
 $username = $env:USERNAME
 $server_directory = "C:\Users\$username\server"
 $eula = "eula=true"
 
+Copy-Item -Path start.ps1 -Destination "C:\Users\$username"
+Copy-Item -Path uninstall.ps1 -Destination "C:\Users\$username"
+
 # Check if Java is installed
 function check_java {
-    $global:javaInstalled = $false
+    $global:java_installed = $false
     Write-Host "Checking Java..."
     $javaPath = Get-Command java -ErrorAction SilentlyContinue
 
     if ($javaPath) {
-        $javaInstalled = $true
+        $global:java_installed = $true
         Write-Host "Java is installed. Java path: $($javaPath.Source)"
     } else {
         Write-Host "Java is not installed."
     }
-
-    return $javaInstalled
 }
 check_java
 
+function transfer_config_files {
+    $global:username = = $env:USERNAME
+    Copy-Item -Path C:\Users\$username\start.ps1 -Destination $server_directory
+    Remove-Item -Path C:\Users\$username\start.ps1
+    Copy-Item -Path C:\Users\$username\uninstall.ps1 -Destination $server_directory
+    Remove-Item -Path C:\Users\$username\uninstall.ps1
+}
+
 # Download the server.jar file from Mojang's official servers
 function server_download {
-    if ($javaInstalled) {
-        if (Test-Path -Path $server_directory -PathType Container) {
-            Write-Host "Erasing contents of $server_directory."
-            Remove-Item -Recurse -Force -Path $server_directory
-            New-Item -ItemType Directory -Path $server_directory
-            Set-Location $server_directory
-            Write-Host "Downloading server.jar for Minecraft $server_version..."
-            Invoke-WebRequest -Uri $server_link -OutFile "server.jar"
-            if ($?) {
-                Write-Host "Download successful."
-            } else {
-                Write-Host "Download failed. Check your internet connection and try again."
-                Exit
-            }
+    if (Test-Path -Path $server_directory -PathType Container) {
+        Write-Host "Erasing contents of $server_directory."
+        Remove-Item -Recurse -Force -Path $server_directory -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 5
+        New-Item -ItemType Directory -Path $server_directory        
+        transfer_config_files
+        Set-Location $server_directory
+        Write-Host "Downloading server.jar for Minecraft $server_version..."
+        Invoke-WebRequest -Uri $server_link -OutFile "server.jar"
+        if ($?) {
+            Write-Host "Download successful."
         } else {
-            Write-Host "Creating proper directories..."
-            New-Item -ItemType Directory -Path $server_directory
-            Set-Location $server_directory
-            Write-Host "Downloading server.jar..."
-            Invoke-WebRequest -Uri $server_link -OutFile "server.jar"
-            if ($?) {
-                Write-Host "Download successful."
-            } else {
-                Write-Host "Download failed. Check your internet connection and try again."
-                Exit
-            }
+            Write-Host "Download failed. Check your internet connection and try again."
+            Exit
         }
     } else {
-        Exit
+        Write-Host "Creating proper directories..."
+        New-Item -ItemType Directory -Path $server_directory
+        transfer_config_files
+        Set-Location $server_directory
+        Write-Host "Downloading server.jar..."
+        Invoke-WebRequest -Uri $server_link -OutFile "server.jar"
+        if ($?) {
+            Write-Host "Download successful."
+        } else {
+            Write-Host "Download failed. Check your internet connection and try again."
+            Exit
+        }
     }
 }
-server_download
+if ($java_installed) {
+    server_download
+}
 
 # Set up the server and accept the EULA
 function server_setup {
@@ -73,8 +84,6 @@ function server_setup {
     Write-Host "Accepting EULA..."
     Out-File -FilePath "$server_directory\eula.txt" -InputObject $eula
     Write-Host "Creating server..."
-    Copy-Item -Path .\start.ps1 -Destination $server_directory
-    Copy-Item -Path .\uninstall.ps1 -Destination $server_directory
 }
 if ($java_installed) {
     server_setup

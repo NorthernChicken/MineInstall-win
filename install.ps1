26ab005942e5531432df59429d7278537a294acc
NorthernChicken <144752748+NorthernChicken@users.noreply.github.com>
2024-01-01 20:34:46 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -10,6 +10,7 @@ $server_version = "1.20.4"
 $server_link = "https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar"
 
 $java_installed = $false
+$username = $env:USERNAME
 $server_directory = "C:\Users\$username\server"
 $eula = "eula=true"
 

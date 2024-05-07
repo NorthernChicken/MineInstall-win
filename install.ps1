31278f9164ce9a7e0a572def904f5e8fc923cd7c
NorthernChicken <144752748+NorthernChicken@users.noreply.github.com>
2024-05-07 06:53:17 -0700

    Update to 1.20.6

--- install.ps1
+++ install.ps1
@@ -6,8 +6,8 @@
 # Repo: https://github.com/NorthernChicken/MineInstall-win
 # -------------------------------
 
-$server_version = "1.20.4"
-$server_link = "https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar"
+$server_version = "1.20.6"
+$server_link = "https://piston-data.mojang.com/v1/objects/145ff0858209bcfc164859ba735d4199aafa1eea/server.jar"
 
 $java_installed = $false
 $username = $env:USERNAME
